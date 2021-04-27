9f7a17cd2c1c3c576638d5bd554286aed221d0af
Dennis Rivera <dennisrivera418@gmail.com>
2021-04-26 20:40:07 -0400

    Completed header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,19 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const Header = () => (
-  <header className='h-20 w-full absolute top-0 z-10 px-10'>
-      <div className='flex items-center justify-between h-20'>
-        <div className="logo">
-          <Link to="/" className='font-bold tracking-wide from-black'>Jungo Codes</Link>
+  <header className='container flex items-center justify-between h-20 w-full absolute top-0 z-10'>
+
+        <div className="logo ml-60">
+          <Link to="/" className='font-bold tracking-wide uppercase from-black'>Jungo Codes</Link>
         </div>
         <div className="navbar">
           <nav className='justify-between mx-5 my-0'>
-            <Link to="/about"> About</Link>
-            <Link to="/work">Work</Link>
-            <Link to="/contact">Contact</Link>
+            <Link to="/about" className='mr-9 uppercase from-black'> About</Link>
+            <Link to="/work" className='mr-9 uppercase from-black'>Work</Link>
+            <Link to="/contact" className='mr-9 uppercase last:mr-0  from-black'>Contact</Link>
           </nav>
         </div>
-      </div>
+
 
   </header>
 )
