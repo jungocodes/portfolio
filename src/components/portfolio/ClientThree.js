559210da0dc6b24a8175f61caa867bbc27972392
Dennis Rivera <dennisrivera418@gmail.com>
2021-05-22 21:24:13 -0400

    completed responsiveness of profile and portfolio component

---
+++ src/components/portfolio/ClientThree.js
@@ -0,0 +1,65 @@
+import * as React from "react"
+import  {Link}  from "gatsby"
+import { FaReadme, FaEye, FaGithubAlt } from "react-icons/fa";
+
+const ClientThree = () => {
+    return (
+        <div class="flex flex-wrap -mx-1  lg:w-3/6 xl:ml-2">
+
+
+<div class=" my-1 px-1 w-full lg:my-4 lg:px-4 ">
+
+
+    <article class="bg-mainAccent overflow-hidden rounded-lg shadow-lg">
+
+
+            <img alt="Placeholder" class="block h-auto w-full" src="https://picsum.photos/600/400/?random"/>
+
+
+        <header class="flex items-center justify-between leading-tight p-4 md:p-6">
+        <h2 className='font-bold text-3xl'><span className='text-secondaryColor'>Jungo</span> Codes</h2>
+        </header>
+
+        <div className=' px-4  flex flex-col items-center -z-50 md:px-6'>
+
+      <p className='mb-4 text-default py-1 leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eveniet, dolorum voluptas incidunt, ipsam aut perferendis nam expedita adipisci consequuntur consequatur autem, eligendi quasi nemo ducimus quod ullam blanditiis possimus.</p>
+    </div>
+
+        <footer class="mx-6 my-6 py-4 flex justify-evenly">
+        <Link
+          to="http://twitter.com/"
+          target="_blank"
+          alt="Project Repo"
+          title="Github"
+          className=" p-2 text-secondaryColor flex items-center uppercase"
+        >
+          <FaGithubAlt className='mr-3 text-lg'/>Code
+        </Link>{" "}
+        <Link
+          to="http://linkedin.com/"
+          target="_blank"
+          title="Live Preview"
+          className=" p-2 text-secondaryColor flex items-center uppercase"
+        >
+          <FaEye className='mr-3 text-lg'/>Live
+        </Link>
+        <Link
+          to="http://linkedin.com/"
+          target="_blank"
+          title="About Project"
+          className=" p-2 text-secondaryColor flex items-center uppercase"
+        >
+          <FaReadme className='mr-3 text-lg'/>Read
+        </Link>
+        </footer>
+
+    </article>
+
+
+</div>
+
+</div>
+    )
+}
+
+export default ClientThree
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,33 +4,28 @@ import { StaticImage } from "gatsby-plugin-image"
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaGithubAlt, FaTerminal } from "react-icons/fa";
 
 
-
-
-
-import "../styles/main.css"
-
-// import HideOnScroll from '../components/HideOnScroll'
-// import BackToTop from '../components/BackToTop'
-
 // Import Components
-// import Portfolio from "../components/portfolio"
+import Portfolio from "../components/portfolio/portfolio"
 // import Contact from "../../contact"
 
 function IndexPage() {
   return (
-    // Container
-    <div className="flex flex-col">
+    <div className='sm:container sm:w-11/12 sm:mx-auto  lg:mt-10 lg:py-6 lg:px-6  '>
+    <div className="flex flex-col lg:mx-auto xl:flex-row xl:items-center xl:w-10/12 xl:p-10 ">
       {/* About Content */}
-       <div className="order-last mx-4 text-center -mt-4">
-        <h1 className='text-4xl font-bold tracking-wider'>
+       <div className="order-last mx-4 text-center -mt-4 lg:mt-0 xl:text-left 2xl:px-8">
+        <h1 className='text-4xl font-bold tracking-wider xl:mt-10 xl:tracking-widest'>
             <Link id="back-to-top-anchor">
 
              <span className='text-secondaryColor'>Jungo</span> Codes
             </Link>
           </h1>
           <h4 className="py-2 tracking-wider">Front-End Developer</h4>
-          <div class="w-12 h-1 bg-secondaryColor rounded mt-2 mb-4 mx-auto"></div>
-          <p className="leading-normal text-lg">
+          <div class="w-12 h-1 bg-secondaryColor rounded mt-2 mb-4 mx-auto xl:m-0"></div>
+          <p className="leading-normal text-lg lg:m-8 xl:m-0 xl:pt-2 2xl:my-4">
+            Meggings portland fingerstache lyft, post-ironic fixie m mi umami.
+          </p>
+          <p className="leading-normal text-lg lg:m-8 xl:m-0 xl:pt-2">
             Meggings portland fingerstache lyft, post-ironic fixie m mi umami
             everyday carry hexagon locavore art party. Locavore small batch
             listicle g m-to-table lumbersexual salvia messenger g book flannel
@@ -38,13 +33,13 @@ function IndexPage() {
             normcore meh butcher.
           </p>
 
-          <div className="mt-4 flex justify-evenly text-xl ">
+          <div className="mt-4 flex justify-evenly text-xl lg:w-8/12 lg:mx-auto xl:justify-start 2xl:w-full">
           <Link
             to="http://twitter.com/"
             target="_blank"
             alt="Twitter Profile"
             title="Twitter"
-            className="rounded-full bg-accentColor p-2 text-mainAccent"
+            className="rounded-full bg-accentColor p-2 text-mainAccent lg:mr-2 2xl:mr-6"
           >
             <FaTwitter />
           </Link>{" "}
@@ -52,7 +47,7 @@ function IndexPage() {
             to="http://linkedin.com/"
             target="_blank"
             title="Linkedin"
-            className="rounded-full bg-accentColor p-2 text-mainAccent"
+            className="rounded-full bg-accentColor p-2 text-mainAccent lg:mr-2 2xl:mr-6"
           >
             <FaLinkedinIn/>
           </Link>{" "}
@@ -60,7 +55,7 @@ function IndexPage() {
             to="http://facebook.com/"
             target="_blank"
             title="Facebook"
-            className="rounded-full bg-accentColor p-2 text-mainAccent"
+            className="rounded-full bg-accentColor p-2 text-mainAccent lg:mr-2 2xl:mr-6"
           >
             <FaFacebookF />
           </Link>{" "}
@@ -68,7 +63,7 @@ function IndexPage() {
             to="http://instagram.com/"
             target="_blank"
             title="Instagram"
-            className="rounded-full bg-accentColor p-2 text-mainAccent"
+            className="rounded-full bg-accentColor p-2 text-mainAccent lg:mr-2 2xl:mr-6"
           >
             <FaInstagram />
           </Link>{" "}
@@ -76,7 +71,7 @@ function IndexPage() {
             to="http://github.com/"
             target="_blank"
             title="Github"
-            className="rounded-full bg-accentColor p-2 text-mainAccent"
+            className="rounded-full bg-accentColor p-2 text-mainAccent lg:mr-2 2xl:mr-6"
           >
             <FaGithubAlt />
           </Link>{" "}
@@ -84,7 +79,7 @@ function IndexPage() {
             to="http://semicolon.dev/"
             target="_blank"
             title="Semicolon"
-            className=" rounded-full bg-accentColor p-2 text-mainAccent"
+            className=" rounded-full bg-accentColor p-2 text-mainAccent lg:mr-2 2xl:mr-6"
           >
             <FaTerminal />
           </Link>{" "}
@@ -92,12 +87,19 @@ function IndexPage() {
 
       </div>
 
-      <div className="">
-        <StaticImage src='../images/Shadow.png' />
+      <div className="mx-auto xl:mt-12">
+        <StaticImage className='lg:w-72 xl:w-96' src='../images/Shadow.png' />
       </div>
 
 
+
     </div>
+
+  {/* Portfolio Section */}
+  <Portfolio />
+
+  </div>
+
   )
 }
 
